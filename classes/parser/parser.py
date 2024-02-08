8e0c079554a35d5c897d0a4e51c7c8c70fd7d69e
justP-official <red-hot-shot@yandex.ru>
2024-02-08 16:44:15 +0300

    v0.3: Добавлена поддержка расширенного поиска

--- classes/parser/parser.py
+++ classes/parser/parser.py
@@ -10,12 +10,16 @@ from bs4 import BeautifulSoup
 
 from classes.post.post import Post
 
+from classes.question.question import Question
+
 
 class Parser:
-    def __init__(self, query):
-        self.query = query
+    def __init__(self, prompt):
+        self.prompt = prompt
 
         self.__session = requests.Session()
+
+        self.filters = self.set_filters()
 
         self.row_data = []
         self.useful_data = []
@@ -28,44 +32,103 @@ class Parser:
     def get_headers():
         headers = {
             "Cookie": 'pwa_disabled=always;',
-            "User-agent": f'{UserAgent.random}',
+            "User-agent": f'{UserAgent.chrome}',
+            'accept': 'application/json',
             "X-Kl-Kfa-Ajax-Request": 'Ajax_Request'
         }
 
         return headers
 
-    def connect(self):
-        print("Подключение...")
-        response = self.session.get(
-            f'https://dtf.ru/search_ajax/v2/content/relevant/1?query={self.query}&mode=raw',
-            headers=self.get_headers())
+    @staticmethod
+    def set_filters():
+        search_filters = {
+            'sort_type': 'relevant',  # если по релевантности, то "relevant" иначе "new"
+            'strict': '',             # если Точное совпадение, то '&strict=1', иначе ''
+            'title': '',              # если По заголовкам, то '&title=1', иначе ''
+            'editorial': '',          # если Материал редакции, то '&editorial=1', иначе ''
+            'blog': '',               # если Личный блог, то '&blog=1', иначе ''
+        }
 
-        return 'application/json' in response.headers.get('content-type') and response.json()['data']['counters']['entries'] > 0
+        is_advanced_search = Question(
+            'Использовать расширенный поиск?\n1. Да;\n2. Нет;',
+            'is_advanced',
+            (True, False)).give_response()
+
+        if is_advanced_search['is_advanced']:
+            questions = (
+                Question(
+                    'Как сортировать записи?\n1. По релевантности;\n2. По дате;',
+                    'sort_type',
+                    ('relevant', 'new')
+                ),
+                Question(
+                    'Точное совпадение\n1. Да;\n2. Нет;',
+                    'strict',
+                    ('&strict=1', '')
+                ),
+                Question(
+                    'По заголовкам\n1. Да;\n2. Нет;',
+                    'title',
+                    ('&title=1', '')
+                ),
+                Question(
+                    'Материал редакции\n1. Да;\n2. Нет;',
+                    'editorial',
+                    ('&editorial=1', '')
+                ),
+                Question(
+                    'Личный блог\n1. Да;\n2. Нет;',
+                    'blog',
+                    ('&blog=1', '')
+                ),
+            )
+
+            for question in questions:
+                response = question.give_response()
+
+                search_filters.update(response)
+
+        return search_filters
+
+    def build_query(self, page_number):
+        query = f'{self.filters["sort_type"]}/{str(page_number)}?query={self.prompt}{self.filters["strict"]}{self.filters["title"]}{self.filters["editorial"]}{self.filters["blog"]}&mode=raw'
+
+        return query
+
+    @staticmethod
+    def connect(response):
+        print("Подключение...")
+
+        return 'application/json' in response.headers.get('content-type')
 
     def search(self):
-        if self.connect():
-            print("Собираем данные...\n")
 
-            counter = 1
+        print("Собираем данные...\n")
 
-            while True:
-                print(f"Часть номер: {counter}")
+        counter = 1
 
-                data = self.session.get(
-                    f'https://dtf.ru/search_ajax/v2/content/relevant/{str(counter)}?query={self.query}&mode=raw',
-                    headers=self.get_headers()).json()
+        while True:
+            url = f"https://dtf.ru/search_ajax/v2/content/{self.build_query(counter)}"
 
-                soup = BeautifulSoup(data['data']['feed_html'], 'lxml')
+            response = self.session.get(url, headers=self.get_headers())
 
-                posts = soup.findAll('div', class_="content-feed")
-                self.row_data += posts
-
-                if not data['data']['is_finished']:
-                    counter += 1
-                else:
+            if counter == 1:
+                if not self.connect(response):
                     break
 
-            print(len(self.row_data))
+            print(f"Часть номер: {counter}")
+
+            data = response.json()
+
+            soup = BeautifulSoup(data['data']['feed_html'], 'lxml')
+
+            posts = soup.findAll('div', class_="content-feed")
+            self.row_data += posts
+
+            if not data['data']['is_finished']:
+                counter += 1
+            else:
+                break
 
     def parse_data(self):
         print("Парсим данные...\n")
@@ -97,7 +160,6 @@ class Parser:
             post_likes_count = int(re.search(r'"count_likes":(\d+)', data.text).group(1))
 
             post_comments_count = int(re.search(r'"count":(\d+)', data.text).group(1))
-
 
             self.useful_data.append(
                 Post(post_title, post_link, post_author, post_date, post_likes_count, post_comments_count)
---
+++ classes/question/question.py
@@ -0,0 +1,22 @@
+class Question:
+    def __init__(self, text: str, key: str, values: tuple):
+        self.text = text
+        self.key = key
+        self.values = values
+
+    def give_response(self):
+        while True:
+            print(self)
+            try:
+                response = int(input(">> ")) - 1
+            except ValueError:
+                print("Неверный ответ. Попробуйте снова.")
+                continue
+
+            if 0 <= response < len(self.values):
+                return {self.key: self.values[response]}
+            else:
+                print("Неверный ответ. Попробуйте снова.")
+
+    def __str__(self):
+        return self.text
